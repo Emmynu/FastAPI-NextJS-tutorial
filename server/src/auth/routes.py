f473085ef038f448e2f868f741f3b670a4e20383
Emmanuel <oketunbi.olufunke@gmail.com>
2026-06-27 08:21:21 -0700

    api interceptors updated

--- server/src/auth/routes.py
+++ server/src/auth/routes.py
@@ -214,7 +214,8 @@ async def logout(resp:Response, userDetails = Depends(tokenBearer)):
                 # domain="http://localhost:3000/"
             )
         # res =  await JtiInBlockList(userDetails["jti"])
-        
+            print("logging out user: ", userDetails)
+
             return { "msg": "Logout successful!"}
    except:
        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="An error occured")
